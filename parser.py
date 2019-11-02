d3ac26d3e921d41437a1a6a1a9341c9f479e898e
ChangHoon-Sung <changhoon.dev@gmail.com>
2019-11-02 08:07:20 +0000

    Fix parsing because of detail search

--- parser.py
+++ parser.py
@@ -1,30 +1,63 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
-import requests, json
+import requests
+import json
 import info
 
-class ISBN_Parser:
+
+class Book_Parser:
     headers = {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
     }
     URL = "http://seoji.nl.go.kr/landingPage/SearchAjax.do"
 
     def __init__(self):
         self.book = dict()
 
-    def parse_bookinfo(self, isbn):
-        self.isbn = isbn
-        data = 'start=0&tSrch_isbn=' + isbn + '&fq_select=tSrch_isbn&q=' + isbn
-
-        res = requests.post(self.URL, headers = self.headers, data=data)
-        
+    def parse(self, data):
+        res = requests.post(self.URL, headers=self.headers,
+                            data=data.encode('utf-8'))
         try:
             if(res.status_code != 200):
                 raise RuntimeError
             else:
-                res = json.loads(res.text)['response']['docs'][0]
-        except:
+                totalCnt = json.loads(res.text)['response']['numFound']
+                docs = json.loads(res.text)['response']['docs']
+
+                print('\n\n')
+                print("총 %d개의 책을 찾았습니다." % totalCnt)
+                candidate = list()
+
+                
+                if(not totalCnt):
+                    print("<< 없거나 등록되지 않은 도서입니다. >>")
+
+                elif(totalCnt < 11):
+                    for idx in range(len(docs)):
+                        candidate.append(docs[idx])
+                    print("검색 결과 상위 10개 항목을 출력합니다.\n\n")
+
+                    for idx, b in enumerate(candidate):
+                        print("%d)" % (idx+1))
+                        self.print_book(b)
+
+                    print("원하는 도서의 번호를 입력해주세요.")
+                    print("ex)3번째 도서면 3 입력")
+                    print("선택 > ")
+
+                    sel = int(input())
+                    return self.simplify(candidate[sel-1])
+                else:
+                    print("찾는 도서가 너무 많습니다!\n수동으로 찾아주세요!")
+                
+                return dict()
+
+        except Exception as e:
+            print(e)
             print("파싱 중 에러 발생!")
+            return dict()
+
+
     def print_book(self, book):
         data = [book['TITLE'], book['AUTHOR'], info.CATEGORY[book['EA_ADD_CODE'][2]],
                 book['PUBLISHER'], book['PUBLISH_PREDATE'], book['EA_ISBN']]
@@ -57,16 +90,24 @@ class ISBN_Parser:
         data = 'start=0&fq_select: tSrch_author&detailSearchYn=Y&fq=' + fq
 
         return self.parse(data)
-        
-      
-      
-    
-        return self.book
+
+
+    def get_info_by_isbn(self, isbn):
+        self.isbn = isbn
+        data = 'start=0&tSrch_isbn=' + isbn + \
+            '&fq_select=tSrch_isbn&q=' + isbn + '&rows=30'
+
+        return self.parse(data)
+
 
 if __name__ == "__main__":
-    p = ISBN_Parser()
-    for n in ('9788934998952', '9791164050444'):
-        b = p.parse_bookinfo(n)
-        for k, v in b.items():
-            print(k, v)
-        print()
+    p = Book_Parser()
+    b = p.get_bookinfo()
+    for k, v in b.items():
+        print(k, v)
+    print()
+
+    # b = p.get_info_by_isbn('9791164357697')
+    # for k, v in b.items():
+    #     print(k, v)
+    # print()
