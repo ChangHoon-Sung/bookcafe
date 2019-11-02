0705e7b2339616479e3fb1ad9eb6b3e8a9596398
ChangHoon-Sung <changhoon.dev@gmail.com>
2019-11-02 08:07:20 +0000

    Add init parser

---
+++ parser.py
@@ -0,0 +1,50 @@
+import requests, json
+import info
+
+class ISBN_Parser:
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+    URL = "http://seoji.nl.go.kr/landingPage/SearchAjax.do"
+
+    def __init__(self):
+        self.book = dict()
+
+    def parse_bookinfo(self, isbn):
+        self.isbn = isbn
+        data = 'start=0&tSrch_isbn=' + isbn + '&fq_select=tSrch_isbn&q=' + isbn
+
+        res = requests.post(self.URL, headers = self.headers, data=data)
+        
+        try:
+            if(res.status_code != 200):
+                raise RuntimeError
+            else:
+                res = json.loads(res.text)['response']['docs'][0]
+        except:
+            print("파싱 중 에러 발생!")
+            return False
+        
+        self.book = {
+            '제목' : res['TITLE'],
+            '분류' : info.CATEGORY[res['EA_ADD_CODE'][2]],
+            '대상' : info.READER[res['EA_ADD_CODE'][0]],
+            '출판사' : res['PUBLISHER'],
+            '발행일' : res['PUBLISH_PREDATE'],
+            'ISBN' : res['EA_ISBN']
+        }
+        
+        # AUTHOR의 Value 예 -> '지은이(저자): 테드 창; 역자(옮긴이) 김상훈;'
+        # 추출할 작가 이름 테드 창
+        sep1 = res['AUTHOR'].find(':')
+        sep2 = res['AUTHOR'].find(';')
+        self.book['저자'] = res['AUTHOR'][sep1+2:sep2]
+    
+        return self.book
+
+if __name__ == "__main__":
+    p = ISBN_Parser()
+    for n in ('9788934998952', '9791164050444'):
+        b = p.parse_bookinfo(n)
+        for k, v in b.items():
+            print(k, v)
